c622e43dff22142105c9869baf179b39fe4082ce
Lorena <lorenapassagem@gmail.com>
2021-01-28 18:42:27 -0300

    :recycle: refatory logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Logo from './components/Logo';
 import Filter from './components/Filter';
 import IconSearch from './components/Search';
+import Header from './components/Header';
 
 function App() {
   return (
