acb51f32b2204bf71a05119f2ff2010fb75921ef
Lorena <lorenapassagem@gmail.com>
2021-01-28 17:31:52 -0300

    :sparkles: include logo img and page logo

---
+++ src/components/Logo/index.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import imgLogo from '../../img/logo.svg';
+
+const Logo = styled.img.attrs({ src: imgLogo, alt:'logo colabCode' })`
+    
+`
+export default Logo;
