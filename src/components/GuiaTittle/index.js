8d2fa62af8ce460e884836df560ea7c2c4606618
Lorena <lorenapassagem@gmail.com>
2021-01-29 16:24:44 -0300

    :recycle: refactory filter and create serviceFilter

--- src/components/GuiaTittle/index.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-const GuiaTittle = styled.p`
-    font-size: 20px;
-    font-family: sans-serif;
-`;
-export default GuiaTittle
---
+++ src/components/ServiceFilter/index.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+const ServiceFilter = styled.nav`
+    background-color: #3a4042;
+`;
+
+export default ServiceFilter;
