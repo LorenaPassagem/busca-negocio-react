2413f3d377e9c496ab3e517278dfbb37dae8b658
Lorena <lorenapassagem@gmail.com>
2021-02-01 18:13:05 -0300

    :lipstick: position service filter

--- src/components/ServiceFilter/styles.js
+++ src/components/ServiceFilter/styles.js
@@ -1,7 +1,13 @@
 import styled from 'styled-components'
+import { Filter } from '../Filter';
 
 const ServiceFilterStyle = styled.nav`
+    display: flex;
     background-color: #3a4042;
+
+    & > ${Filter} {
+        padding-right: 20px;
+    }
 `;
 
 export default ServiceFilterStyle;
