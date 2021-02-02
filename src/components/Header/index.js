8d7c4341ea0cfa0e96fa9d538e1a461843bb4509
Lorena <lorenapassagem@gmail.com>
2021-02-01 21:10:46 -0300

    :lipstick: space itens header and serviceFilter

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,5 +4,6 @@ const Header = styled.header`
     background-color: #395164;
     display: flex;
     justify-content: space-between;
+    padding: 1rem
 `;
 export default Header;
--- src/components/ServiceFilter/styles.js
+++ src/components/ServiceFilter/styles.js
@@ -3,7 +3,9 @@ import { Filter } from '../Filter';
 
 const ServiceFilterStyle = styled.nav`
     display: flex;
+    align-items: center;
     background-color: #3a4042;
+    padding: 10px;
 
     & > ${Filter} {
         padding-right: 20px;
