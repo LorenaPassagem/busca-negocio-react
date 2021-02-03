921958e4c4f86c34c02e74c2500becafef1acb3c
Lorena <lorenapassagem@gmail.com>
2021-02-03 14:26:21 -0300

    :sparkles: create components serviceFilter

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import IconFilter from '../../img/filter.svg';
 
-export const Filter = styled.img.attrs({src: IconFilter, alt:'icom para filtro'})`
+export const Filter = styled.img.attrs({src: IconFilter, alt:'icon para filtro'})`
 `;
 
 export const Label = styled.p`
