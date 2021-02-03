ccd9147eb28136ac4cf3aaf8f01813640c2aae0a
Lorena <lorenapassagem@gmail.com>
2021-02-03 15:24:51 -0300

    :sparkles:  change story's background

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+
+  backgrounds: {
+    default: 'dark',
+    values: [
+      {
+        name: 'dark',
+        value: '#3c5268',
+      },
+
+    ],
+  },
+};
