6bf266e584f8ba23921bc5f6d1fb765c38180856
Lorena <lorenapassagem@gmail.com>
2021-02-05 11:35:59 -0300

    :sparkles: create button insira negocio

---
+++ src/components/Button/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+    background-color: #f25a70;
+    border-style: none;
+    padding: var(--spacing-small);
+    width:350px;
+`
+export default Button
