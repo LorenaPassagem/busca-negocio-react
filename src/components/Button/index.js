c58191cff5dbc47ef5aa78746533debaecbf5a5e
Lorena <lorenapassagem@gmail.com>
2021-02-05 16:11:11 -0300

    :sperkles: create button insira seu negocio in storybook

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,8 @@
 import styled from 'styled-components'
 
-const Button = styled.button`
+export const Button = styled.button`
     background-color: #f25a70;
     border-style: none;
     padding: var(--spacing-small);
     width:350px;
 `
-export default Button
